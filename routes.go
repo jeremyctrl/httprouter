f6080f5a1889e9a8cd0efec6013c63012db1b863
Jeremy <235932977+jeremyctrl@users.noreply.github.com>
2025-11-08 00:44:52 +0200

    feat: implement 'build' method

--- routes.go
+++ routes.go
@@ -1,6 +1,8 @@
 package httprouter
 
 import (
+	"strings"
+
 	"github.com/cespare/mph"
 )
 
@@ -25,7 +27,46 @@ type mphGroup struct {
 type mphResult map[int]mphGroup
 
 func build(routes []routeDef) mphResult {
+	grouped := make(map[int][]routeCompiled)
 
+	for _, def := range routes {
+		var paramIndices []int
+
+		segments := strings.Split(def.path, "")
+		for i, segment := range segments {
+			if strings.HasPrefix(segment, ":") {
+				paramIndices = append(paramIndices, i)
+				segments[i] = ":"
+			}
+		}
+
+		normalized := def.method + ":" + strings.Join(segments, "/")
+		depth := len(segments)
+
+		grouped[depth] = append(grouped[depth], routeCompiled{
+			method:       def.method,
+			key:          normalized,
+			paramIndices: paramIndices,
+			handler:      def.handler,
+		})
+	}
+
+	groups := make(map[int]mphGroup)
+
+	for depth, routes := range grouped {
+		keys := make([]string, len(routes))
+		for i, rc := range routes {
+			keys[i] = rc.key
+		}
+
+		table := mph.Build(keys)
+		groups[depth] = mphGroup{
+			table:  table,
+			routes: routes,
+		}
+	}
+
+	return groups
 }
 
 func find(groups mphResult, method, path string) (*routeCompiled, []string) {
