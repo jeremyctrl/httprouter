843a39fec2f6857c537209674bd9f5ba3ba19251
Jeremy <235932977+jeremyctrl@users.noreply.github.com>
2025-11-07 23:40:58 +0200

    feat(router): skeleton

---
+++ router.go
@@ -0,0 +1,48 @@
+package httprouter
+
+import "net/http"
+
+type Handler func(w http.ResponseWriter, req *http.Request, params ...string)
+
+type Router struct {
+}
+
+func New() *Router {
+	return &Router{}
+}
+
+func (r *Router) GET(path string, handler Handler) *Router {
+	return r.Handle(http.MethodGet, path, handler)
+}
+
+func (r *Router) HEAD(path string, handler Handler) *Router {
+	return r.Handle(http.MethodHead, path, handler)
+}
+
+func (r *Router) POST(path string, handler Handler) *Router {
+	return r.Handle(http.MethodPost, path, handler)
+}
+
+func (r *Router) PUT(path string, handler Handler) *Router {
+	return r.Handle(http.MethodPut, path, handler)
+}
+
+func (r *Router) DELETE(path string, handler Handler) *Router {
+	return r.Handle(http.MethodDelete, path, handler)
+}
+
+func (r *Router) OPTIONS(path string, handler Handler) *Router {
+	return r.Handle(http.MethodOptions, path, handler)
+}
+
+func (r *Router) PATCH(path string, handler Handler) *Router {
+	return r.Handle(http.MethodPatch, path, handler)
+}
+
+func (r *Router) Handle(method, path string, handler Handler) *Router {
+	return r
+}
+
+func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+
+}
